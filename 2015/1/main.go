cc5c271f7c5b3015de7972340ff66bee877eb8ea
Jay Shah <reversearrow@gmail.com>
2020-04-04 18:09:27 -0400

    WIP

---
+++ 2015/1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func findDirections(p string) int {
+	startingFloor := 0
+	floorsInput := strings.Split(p, "")
+	for _, v := range floorsInput {
+		if v == "(" {
+			startingFloor++
+		} else if v == ")" {
+			startingFloor--
+		}
+	}
+	return startingFloor
+}
+
+func main() {
+	direction := findDirections("()(")
+	fmt.Println(direction)
+}
